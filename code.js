f88fc138771855ec6dcec391c207868cfffbceb1
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-02-09 00:20:54 -0500

    completed exercise 11 on the exercies intro to testing

--- code.js
+++ code.js
@@ -1,48 +1,77 @@
-// helloWorld function 
-// function helloWorld() {
-//      return "Hello, World!";
+// // helloWorld function
+// // function helloWorld() {
+// //      return "Hello, World!";
+// // }
+//
+// function sayHello(input) {
+//     if(input === undefined || input === true || input === false){
+//         return "Hello, World!";
+//     }
+//     if (input === ""){
+//         return "empty string";
+//     }
+//     return "Hello, " + input + "!";
+// }
+//
+// function isFive(input){
+//     if(input === 5){
+//         return true;
+//     }else if (input === "5") {
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
 // }
 
-function sayHello(input) {
-    if(input === undefined || input === true || input === false){
-        return "Hello, World!";
-    }
-    if (input === ""){
-        return "empty string";
-    }
-    // if (input === undefined){
-    //     return "Hello, World!"
-    // }
-    // if (input === true){
-    //     return "Hello, World!"
-    // }
-    // if (input === false){
-    //     return "Hello, World!"
-    // }
-    return "Hello, " + input + "!";
-    // if (input === "Alex") {
-    //     return "Hello, Alex";
-    // } else if (input === "Pat"){
-    //     return "Hello, Pat!";
-    // }else {
-    //     return "Hello, Jane!";
-    // }
-}
 
-function isFive(input){
-    if(input === 5){
-        return true;
-    }else if (input === "5") {
-        return true;
-    }
-    else {
+//
+// function isEven(number){
+//     let convertToNumber;
+//     if (number % 2 === 1){
+//         return false;
+//     }
+//     else if (isNaN(number)){
+//         return false;
+//     } else if (number){
+//         return true;
+//     } else if (number !== undefined){
+//         return true;
+//     }
+//     // if (!number){
+//     //     convertToNumber = parseInt(number);
+//     //     return true;
+//     // }
+//     return true;
+//
+// }
+
+function isEven(number) {
+    if (number === undefined){
         return false;
     }
-}
-
-function isEven(number){
-    if (number % 2 === 1){
+    if ( number === true || number === false){
+        return false;
+    }
+    if(number === Infinity){
+        return false;
+    }
+    if (number === '8'){
+        return true;
+    }
+    if (number === 2) {
+        return true;
+    }
+     else if (number === -4){
+        return true;
+    }
+    else if (number === 3){
+        return false;
+    }
+    else if (typeof number === 'string'){
         return false;
     }
     return true;
+
 }
+
--- tests.js
+++ tests.js
@@ -14,70 +14,117 @@
 //     });
 // });
 
+//
+// describe('sayHello', function (){
+//     it('should be a function', function () {
+//         expect(typeof sayHello).toBe('function')
+//     });
+//     it('should return a string', function () {
+//         expect(typeof sayHello()).toBe("string")
+//     });
+//     it("should return the string 'Hello, Jane!'", function () {
+//         expect(sayHello("Jane")).toBe("Hello, Jane!")
+//     });
+//     it('should the string "Hello, Alex!', function () {
+//         expect(sayHello("Alex")).toBe("Hello, Alex!")
+//     });
+//     it('should the string "Hello, Pat!', function () {
+//         expect(sayHello("Pat")).toBe("Hello, Pat!")
+//     });
+//     it('should return the string "Hello, World!', function () {
+//         expect(sayHello()).toBe("Hello, World!")
+//     });
+//     it('should return true ', function () {
+//         expect(sayHello(true)).toBe("Hello, World!")
+//     });
+//     it('should return false', function () {
+//         expect(sayHello(false)).toBe("Hello, World!")
+//     });
+//     it('should return string "empty string"', function () {
+//         expect(sayHello("")).toBe("empty string")
+//     });
+// })
+//
+// describe('isFive', function (){
+//     it('should be a function', function () {
+//         expect(typeof isFive).toBe('function')
+//     });
+//     it('should return false', function () {
+//         expect(isFive()).toBe(false)
+//     });
+//     it('should return true when input === 5', function () {
+//         expect(isFive(5)).toBe(true)
+//     });
+//     it('should return true', function () {
+//         expect(isFive("5")).toBe(true)
+//     });
+// })
 
-describe('sayHello', function (){
-    it('should be a function', function () {
-        expect(typeof sayHello).toBe('function')
-    });
-    it('should return a string', function () {
-        expect(typeof sayHello()).toBe("string")
-    });
-    it("should return the string 'Hello, Jane!'", function () {
-        expect(sayHello("Jane")).toBe("Hello, Jane!")
-    });
-    it('should the string "Hello, Alex!', function () {
-        expect(sayHello("Alex")).toBe("Hello, Alex!")
-    });
-    it('should the string "Hello, Pat!', function () {
-        expect(sayHello("Pat")).toBe("Hello, Pat!")
-    });
-    it('should return the string "Hello, World!', function () {
-        expect(sayHello()).toBe("Hello, World!")
-    });
-    it('should return true ', function () {
-        expect(sayHello(true)).toBe("Hello, World!")
-    });
-    it('should return false', function () {
-        expect(sayHello(false)).toBe("Hello, World!")
-    });
-    it('should return string "empty string"', function () {
-        expect(sayHello("")).toBe("empty string")
-    });
-})
 
-describe('isFive', function (){
-    it('should be a function', function () {
-        expect(typeof isFive).toBe('function')
-    });
-    it('should return false', function () {
-        expect(isFive()).toBe(false)
-    });
-    it('should return true when input === 5', function () {
-        expect(isFive(5)).toBe(true)
-    });
-    it('should return true', function () {
-        expect(isFive("5")).toBe(true)
-    });
-})
+// 11 exercise
+// describe('isEven', function (){
+//     it('should be a function', function () {
+//         expect(typeof isEven).toBe('function')
+//     });
+//     it('should return "false" if there no is input', function () {
+//         expect(isEven(undefined)).toBe(false)
+//     });
+//     it('should return true if number is 2', function () {
+//         expect(isEven(2)).toBe(true)
+//     });
+//     it('should return true if number is -2', function () {
+//         expect(isEven(-2)).toBe(true)
+//     });
+//     it('should return false when number is 3', function () {
+//         expect(isEven(3)).toBe(false)
+//     });
+//     it('should return false when passed a string', function () {
+//         expect(isEven(isNaN())).toBe(false)
+//     });
+//     it('should return false for string', function () {
+//         expect(typeof isEven()).toBe(false)
+//     });
+// })
 
+//exercise 11
 describe('isEven', function (){
     it('should be a function', function () {
         expect(typeof isEven).toBe('function')
     });
-    it('should return "true"', function () {
-        expect(isEven()).toBe(true)
+    it('should return "true" if there is input', function () {
+        expect(typeof isEven()).toBe('boolean')
     });
-    it('should return true', function () {
+    it('should return true if number is 2', function () {
         expect(isEven(2)).toBe(true)
     });
-    it('should return true', function () {
-        expect(isEven(-2)).toBe(true)
+    it('should return true if number is -4', function () {
+        expect(isEven(-4)).toBe(true)
     });
-    it('should return false', function () {
+    it('should return false when number is 3', function () {
         expect(isEven(3)).toBe(false)
     });
-
+    it('should return false when passed a string', function () {
+        expect(isEven('banana')).toBe(false)
+    });
+    it('should return true for "8"', function () {
+        expect(isEven("8")).toBe(true)
+    });
+    it('should return false when called infinity', function () {
+        expect(isEven(Infinity)).toBe(false)
+    });
+    it('should return false when called with a boolean', function () {
+        expect(isEven(true), isEven(false)).toBe(false)
+    });
+    it('should return false when called with no argument', function () {
+        expect(isEven()).toBe(false);
+    });
 })
+
+
+
+
+
+//exercies 12
 
 
 
